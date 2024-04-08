5f62753153274e3c04dc8cc49fb38bdfc4470e72
PranavModale <27pranavmod@gmail.com>
2024-04-08 12:42:07 +0530

    Started working

---
+++ app/src/main/java/com/example/greenharvest/admin/AdminMain.java
@@ -0,0 +1,19 @@
+package com.example.greenharvest.admin;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.greenharvest.R;
+
+public class AdminMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+
+    }
+}
