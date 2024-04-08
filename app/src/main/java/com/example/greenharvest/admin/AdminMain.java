f8dfe46a33de5b1990b0434c42386ca2a0c3037a
rohitpatill <rp2876741@gmail.com>
2024-04-09 00:51:19 +0530

    SingIn and SingUp Modules done

--- app/src/main/java/com/example/greenharvest/admin/AdminMain.java
+++ app/src/main/java/com/example/greenharvest/admin/AdminMain.java
@@ -1,19 +1,76 @@
 package com.example.greenharvest.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.greenharvest.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AdminMain extends AppCompatActivity {
+
+    private BottomNavigationView bnView;
+    private MenuItem prevMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
 
+        bnView = findViewById(R.id.bottomNavigationView);
+        bnView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
+                    selectedFragment = new A_Admin();
+                } else if (id == R.id.nav_container_box) {
+                    selectedFragment = new B_Admin();
+                } else if (id == R.id.nav_add) {
+                    selectedFragment = new C_Admin();
+                }else if (id == R.id.nav_profile) {
+                    selectedFragment = new D_Admin();
+                }
+
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+
+                return true;
+            }
+        });
+
+        // Set the home fragment as the default fragment
+        loadFragment(new A_Admin());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_layout, fragment);
+        ft.commit();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // Clear the activity stack and go to the device's home screen
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
